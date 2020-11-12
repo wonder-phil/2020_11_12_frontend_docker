b7924edb754c21deab541cc600c1e7fddf11a3e6
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2020-11-12 12:15:40 -0500

    cleaned up

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,6 @@ const router = express.Router();
 
 function no_cors_setup(res) {
 
-  //Access-Control-Allow-Origin
-  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000');
-  //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Origin', '*');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -35,7 +32,7 @@ var index = 0;
 
 /*
  *
- * curl http://localhost:3001/btc_date_equal?date=2020-05
+ * curl http://localhost:3009/get_data
 */
 router.get('/get_data', async (req,res, next) => {
   no_cors_setup(res);
